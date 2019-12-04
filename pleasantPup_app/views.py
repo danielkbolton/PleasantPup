e6690cc3d8a6904317a447b936bf676cbb2cc71b
DanielB <danielkbolton@gmail.com>
2019-12-04 12:05:24 -0800

    adds base, landing, and about templates

--- pleasantPup_app/views.py
+++ pleasantPup_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+
+
+def landing(request):
+    return render(request, 'landing.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+""" def home(request):
+    return render(request, 'home.html') """
